d1e5fdcb89498b4e78827b326ccfa80fb8db125b
rishabhiitbhu <rishabh.agrahari.eee15@iitbhu.ac.in>
2018-01-19 14:22:21 +0530

    Data scrapping done

---
+++ get_real_time_img.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+base_url = 'https://www.delhisldc.org'
+
+url = base_url + '/Loadcurve.aspx'
+
+resp = requests.get(url)
+soup = BeautifulSoup(resp.text, 'lxml')
+img_link = soup.find('img', {'id': 'ContentPlaceHolder2_chartcontrol1'})['src']
+print(img_link)
+url = base_url + img_link
+
+f = open('real_time.png','wb')
+f.write(requests.get(url).content)
+f.close()
