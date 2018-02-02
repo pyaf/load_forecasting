f61e6a36dfefc44ccdaebe742867f694202e22cc
rishabhiitbhu <rishabh.agrahari.eee15@iitbhu.ac.in>
2018-02-02 12:09:00 +0530

    bugfix

--- whether_scrap.py
+++ whether_scrap.py
@@ -17,27 +17,27 @@ year_range = [2017, 2018]
 if not os.path.exists('Whether_Data'):
 	    os.makedirs('Whether_Data')
 
-columns = ['time', 'temp', 'heat index', 'dew point', 'humidity',
-			 'pressure', 'visibility', 'wind dir', 'wind speed', 
-			 'gust speed', 'precip', 'events', 'conditions']
+
 
 for year in year_range:
 	for month in month_range[year]:
 		month_dir = 'Whether_Data/%d/%02d/' %(year, month)
 		if not os.path.exists(month_dir): os.makedirs(month_dir)
-		try:
-			for day in day_range:
+		for day in day_range:
+			try:
 				date = '%02d/%02d/%d' %(day, month, year)
 				print('Scraping', date)
-				resp = requests.get(url % (year, month, day)) # send a get request to the url, get response
+				current_url = url % (year, month, day)
+				resp = requests.get(current_url) # send a get request to the url, get response
 				soup = BeautifulSoup(resp.text, 'lxml') # Yummy HTML soup
 				table = soup.find('table', {'id':'obsTable'}) # get the table from html
 				trs = table.findAll('tr') # extract all rows of the table
-				if len(trs[1:])!=0: # no need to create csv file, if there's no data, for Aug month of 2017
+				if len(trs[1:])!=0:
 					csv_filename = month_dir + '%s.csv' % date.replace('/', '-')
 					if os.path.exists(csv_filename): os.remove(csv_filename) # remove the file it already exists, can result in data duplicacy
 					with open(csv_filename, 'a') as f:
 						writer = csv.writer(f)
+						columns = [th.text for th in trs[0].findChildren('th')]					
 						writer.writerow(columns)
 						for tr in trs[1:]:
 							row = []
@@ -48,8 +48,9 @@ for year in year_range:
 									row.append(span[0].text.strip())
 								else:
 									row.append(td.text.strip())
-							assert (len(row) == len(columns))
+							assert len(row) == len(columns)
 							writer.writerow(row)
-		except Exception as e:
-			print(e)
-			print(date)
+			except Exception as e:
+				print('Exception', e)
+				print(date)
+				print(current_url)
