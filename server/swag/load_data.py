892feab999523c3d24e92a6fedced8bbac2e95f1
Ayush Goyal <36241930+agl29@users.noreply.github.com>
2018-03-26 19:31:37 +0530

    adding code for updating database (#9)
    * Removed redis source code
    
    * Removing extra files and try to Give appropriate naming of function and folders
    
    * trying to resolve conflict
    
    * templet changes()
    
    * adding SMAVG and WMAVG
    
    * adding scrap.py for updating database

--- server/swag/load_data.py
+++ server/swag/load_data.py
@@ -5,10 +5,25 @@ django.setup()
 from swag.models import CSV
 import csv, datetime
 
-for i in range(22,24):
-    a = 1+i;
-    print(a);
-    link = "static/SLDC_Data/2018/03/"+str(a).zfill(2)+"-03-2018.csv"
-    dataReader = csv.reader(open(link), delimiter=',', quotechar='"')
-    for row in dataReader:
-        data = CSV.objects.create(timestamp = row[0], load_value = row[1], date = datetime.date(2018,3,a))
+day_range = list(range(0, 31)) # days, 1 to 31
+
+# months, Aug to Dec for 2017, and Jan for 2018
+month_range = {
+				2017: [8,9,10,11,12],
+				2018: [1,2,3]
+				}
+
+year_range = [2017,2018]
+
+for year in year_range:
+	for month in month_range[year]:
+			for day in day_range:
+				try:
+					a = 1+day
+					print(a,month,year);
+					link = "static/SLDC_Data/"+str(year)+"/"+str(month).zfill(2)+"/"+str(a).zfill(2)+"-"+str(month).zfill(2)+"-"+str(year)+".csv"
+					dataReader = csv.reader(open(link), delimiter=',', quotechar='"')
+					for row in dataReader:
+						data = CSV.objects.create(timestamp = row[0], load_value = row[1], date = datetime.date(2018,3,a))
+				except Exception as e:
+					print(e)
