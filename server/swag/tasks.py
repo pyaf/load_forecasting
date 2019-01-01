b9a3599be2214bb6823672095e37b1f6d641906c
Rishabh Agrahari <rishabh.agrahari.eee15@iitbhu.ac.in>
2019-01-01 19:16:38 +0530

    Bugfix in scheduler

--- server/swag/tasks.py
+++ server/swag/tasks.py
@@ -1,3 +1,4 @@
+from .models import CSV
 from celery.task.schedules import crontab
 from celery.decorators import periodic_task
 
@@ -12,179 +13,89 @@ import csv
 import os
 from bs4 import BeautifulSoup
 
-import sys,os
-djangoproject_home="website"
+import sys
+import os
+djangoproject_home = "website"
 sys.path.append(djangoproject_home)
 os.environ['DJANGO_SETTINGS_MODULE'] = 'website.settings'
-from .models import CSV
-import csv,datetime
 
-# from .views import forecasted_plot
-
-# @periodic_task(run_every=(crontab(minute='*/1')), name="update_database", ignore_result=True
-
-# size=0
 
 @periodic_task(run_every=(crontab(minute=55, hour='0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23')), name="reload_task", ignore_result=True)
 def reload_task():
-	# global size
-	print("date dekh le bhai",datetime.date.today().day)
-	url = 'http://www.delhisldc.org/Loaddata.aspx?mode='
-	day_range = list(range(datetime.date.today().day,datetime.date.today().day+1)) # days, 1 to 31
-	# months, Aug to Dec for 2017, and Jan for 2018
-	month_range = {
-					# 2017: list(range(12, 13)),
-					2018: [datetime.date.today().month]
-					}
+    # global size
+    print("date:", datetime.date.today().day)
+    url = 'http://www.delhisldc.org/Loaddata.aspx?mode='
+    day_range = list(range(datetime.date.today().day,
+                           datetime.date.today().day+1))  # days, 1 to 31
+    # months, Aug to Dec for 2017, and Jan for 2018
+    
+    month = datetime.date.today().month
+    year = datetime.date.today().year
 
-	year_range = [datetime.date.today().year]
+    if not os.path.exists('static/SLDC_Data'):
+        os.makedirs('static/SLDC_Data')
 
-	if not os.path.exists('static/SLDC_Data'):
-		os.makedirs('static/SLDC_Data')
+    month_dir = 'static/SLDC_Data/%d/%02d/' % (year, month)
+    if not os.path.exists(month_dir):
+        os.makedirs(month_dir)
+    try:
+        for day in day_range:
+            date = '%02d/%02d/%d' % (day, month, year)
+            print('Scraping', date)
+            # send a get request to the url, get response
+            resp = requests.get(url+date)
+            soup = BeautifulSoup(resp.text, 'lxml')  # Yummy HTML soup
+            # get the table from html
+            table = soup.find(
+                'table', {'id': 'ContentPlaceHolder3_DGGridAv'})
+            trs = table.findAll('tr')  # extract all rows of the table
+            # no need to create csv file, if there's no data, for Aug month of 2017
+            if len(trs[1:]) != 0:
+                csv_filename = month_dir + \
+                    '%s.csv' % date.replace('/', '-')
+                if os.path.exists(csv_filename):
+                    # remove the file it already exists, can result in data duplicacy
+                    os.remove(csv_filename)
+                with open(csv_filename, 'a') as f:
+                    writer = csv.writer(f)
+                    for tr in trs[1:]:
+                        time, delhi = tr.findChildren('font')[:2]
+                        t = (time.text).split(':')
+                        TimE = datetime.time(int(t[0]), int(t[1]))
+                        writer.writerow([str(TimE), delhi.text])
+                        print(str(TimE))
+                        results = []
+                        results.append(CSV.objects.filter(
+                            date=datetime.date.today(), timestamp=str(TimE)))
+                        if (len(results[0]) == 0):
+                            print(TimE, delhi.text)
+                            data = CSV()
+                            data.timestamp = str(TimE)
+                            data.load_value = delhi.text
+                            data.date = datetime.date.today()
+                            data.save()
 
-	for year in year_range:
-		for month in month_range[year]:
-			month_dir = 'static/SLDC_Data/%d/%02d/' %(year, month)
-			if not os.path.exists(month_dir): os.makedirs(month_dir)
-			try:
-				for day in day_range:
-					date = '%02d/%02d/%d' %(day, month, year)
-					print('Scraping', date)
-					resp = requests.get(url+date) # send a get request to the url, get response
-					soup = BeautifulSoup(resp.text, 'lxml') # Yummy HTML soup
-					table = soup.find('table', {'id':'ContentPlaceHolder3_DGGridAv'}) # get the table from html
-					trs = table.findAll('tr') # extract all rows of the table
-					if len(trs[1:])!=0: # no need to create csv file, if there's no data, for Aug month of 2017
-						csv_filename = month_dir + '%s.csv' % date.replace('/', '-')
-						if os.path.exists(csv_filename): os.remove(csv_filename) # remove the file it already exists, can result in data duplicacy
-						with open(csv_filename, 'a') as f:
-							writer = csv.writer(f)
-							# writer.writerow(['x', 'y'])
-							for tr in trs[1:]:
-								time, delhi = tr.findChildren('font')[:2]
-								t = (time.text).split(':')
-								TimE = datetime.time(int(t[0]),int(t[1]))
-								writer.writerow([str(TimE), delhi.text])
-								print(str(TimE))
-								results=[]
-								results.append(CSV.objects.filter(date = datetime.date.today(), timestamp = str(TimE)))
-								# print("hhhhhhhhhhheeeee", len(results[0]))
-								if (len(results[0]) == 0):
-									# for z in results[0]:
-									#	print (z.timestamp, type(z.timestamp), len(results))
-									print(TimE, delhi.text)
-									data = CSV()
-									data.timestamp = str(TimE)
-									data.load_value = delhi.text
-									data.date = datetime.date.today()
-									data.save()
+                    if(datetime.datetime.time(datetime.datetime.now()).hour == 23 and datetime.datetime.time(datetime.datetime.now()).minute >= 55):
+                        print(
+                            '............................................')
+                        start = datetime.time(0, 0)
+                        end = datetime.time(23, 55)
+                        while(start != end):
+                            blanck_val = []
+                            blanck_val.append(CSV.objects.filter(
+                                date=datetime.date.today(), timestamp=str(start)))
+                            if (len(blanck_val[0]) == 0):
+                                data = CSV()
+                                data.timestamp = str(start)
+                                data.load_value = None
+                                data.date = datetime.date.today()
+                                data.save()
+                                writer.writerow([str(start), None])
+                                # CSV.objects.filter(date = datetime.date.today(), timestamp = str(start)).update(load_value = None)
+                            start = (datetime.datetime.combine(datetime.date(
+                                1, 1, 1), start)+timedelta(minutes=5)).time()
 
-							if(datetime.datetime.time(datetime.datetime.now()).hour == 23 and datetime.datetime.time(datetime.datetime.now()).minute >= 55):
-								print('............................................')
-								# x = '00:00:00'
-								start = datetime.time(0,0)
-								end = datetime.time(23,55)
-								# size_reduction = 0
-								while(start != end):
-									# tym = x.split(':')
-									# Time = datetime.time(int(tym[0]),int(tym[1]))
-									# print((datetime.datetime.combine(datetime.date(1,1,1),Time)+timedelta(minutes=5)).time())
-									blanck_val=[]
-									blanck_val.append(CSV.objects.filter(date = datetime.date.today(), timestamp = str(start)))
-									# print("hhhhhhhhhhheeeee", len(results[0]))
-									if (len(blanck_val[0]) == 0):
-										data = CSV()
-										data.timestamp = str(start)
-										data.load_value = None
-										data.date = datetime.date.today()
-										data.save()
-										writer.writerow([str(start), None])
-										# CSV.objects.filter(date = datetime.date.today(), timestamp = str(start)).update(load_value = None)
-									start = (datetime.datetime.combine(datetime.date(1,1,1),start)+timedelta(minutes=5)).time()
-
-			except Exception as e:
-				print(e)
-
-	# link = "static/SLDC_Data/"+ str(year) + "/"+ str(month).zfill(2) + "/" + str(day).zfill(2) + "-" + str(month).zfill(2) + "-" + str(year) +".csv"
-	# # link = "static/SLDC_DataBase/2017/11/"+str(a).zfill(2)+"-11-2017.csv"
-	# dataReader = csv.reader(open(link), delimiter=',', quotechar='"')
-	# rows = list(dataReader)
-	# print(len(rows))
-	# print(rows)
-	# print("global k baad",size)
-	# # if(len(rows) > size):
-	# print(len(rows))
-	# print(size_reduction)
-	# for row in rows[size:len(rows)+1]:
-	# 	print("inside 2")
-	# 	data = CSV()
-	# 	if(row[1]=='None'):
-	# 		break
-	# 	print('row[0]',row[0])
-	# 	data.timestamp = row[0]
-	# 	data.load_value = row[1]
-	# 	data.date = datetime.date(datetime.date.today().year,datetime.date.today().month,datetime.date.today().day)
-	# 	data.save()
-	# size = len(rows)-size_reduction
-	# if(datetime.datetime.time(datetime.datetime.now()).hour == 23 and datetime.datetime.time(datetime.datetime.now()).minute >= 40):
-	# 	size = 0
-	# print("final wali........end",size)
-	# print("update_done...............yupppp")
+    except Exception as e:
+        print(e)
 
 
-
-# def update_database():
-# 	print("qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq")
-	# for i in range(30):
-	# 	a = 1+i;
-	# 	print(a);
-	# print("000000000000000000000000mmmmmmmmmmm")
-	# print("adscvadsfvadsfvadsfa")
-	# print("date dekh le bhai",datetime.date.today().day)
-	# url = 'https://www.delhisldc.org/Loaddata.aspx?mode='
-	# day_range = list(range(datetime.date.today().day,datetime.date.today().day+1)) # days, 1 to 31
-	# # months, Aug to Dec for 2017, and Jan for 2018
-	# month_range = {
-	#                 # 2017: list(range(12, 13)),
-	#                 2018: [datetime.date.today().month]
-	#                 }
-
-	# year_range = [datetime.date.today().year]
-
-	# if not os.path.exists('static/SLDC_DataBase'):
-	#         os.makedirs('static/SLDC_DataBase')
-
-	# for year in year_range:
-	#     for month in month_range[year]:
-	#         month_dir = 'static/SLDC_DataBase/%d/%02d/' %(year, month)
-	#         if not os.path.exists(month_dir): os.makedirs(month_dir)
-	#         try:
-	#             for day in day_range:
-	#                 date = '%02d/%02d/%d' %(day, month, year)
-	#                 print('Scraping', date)
-	#                 resp = requests.get(url+date) # send a get request to the url, get response
-	#                 soup = BeautifulSoup(resp.text, 'lxml') # Yummy HTML soup
-	#                 table = soup.find('table', {'id':'ContentPlaceHolder3_DGGridAv'}) # get the table from html
-	#                 trs = table.findAll('tr') # extract all rows of the table
-	#                 if len(trs[1:])!=0: # no need to create csv file, if there's no data, for Aug month of 2017
-	#                     csv_filename = month_dir + '%s.csv' % date.replace('/', '-')
-	#                     if os.path.exists(csv_filename): os.remove(csv_filename) # remove the file it already exists, can result in data duplicacy
-	#                     with open(csv_filename, 'a') as f:
-	#                         writer = csv.writer(f)
-	#                         # writer.writerow(['x', 'y'])
-	#                         for tr in trs[1:]:
-	#                             time, delhi = tr.findChildren('font')[:2]
-	#                             writer.writerow([time.text, delhi.text])
-	#         except Exception as e:
-	#             print(e)
-	# print("done updating")
-	# link = "/static/SLDC_DataBase/"+ year + "/"+ month + "/" + day + "-" + month + "-" + year +".csv"
-	# # link = "static/SLDC_DataBase/2017/11/"+str(a).zfill(2)+"-11-2017.csv"
-	# dataReader = csv.reader(open(link), delimiter=',', quotechar='"')
-	# for row in dataReader:
-	# 	data = CSV()
-	# 	data.timestamp = row[0]
-	# 	data.load_value = row[1]
-	# 	data.date = datetime.date(datetime.date.today().year,datetime.date.today().month,datetime.date.today().day)
-	# 	data.save()
-	# print("update_done...............yupppp")
--- server/swag/views.py
+++ server/swag/views.py
@@ -12,10 +12,8 @@ import pandas as pd
 from .models import CSV
 
 
-
 def home_page(request):
-    print("adscvadsfvadsfvadsfa")
-    print("date dekh le bhai",datetime.date.today().day)
+    print("date:", datetime.date.today().day)
     day = datetime.date.today().day
     month = datetime.date.today().month
     year = datetime.date.today().year
@@ -25,31 +23,23 @@ def home_page(request):
 
 
 def graph_plot(request):
-    print("insde graph_plot");
     if request.method == 'POST':
-        print(request.POST)
-        # query_results = CSV.objects.filter(date = request.POST['from'])
+        # print(request.POST)
         a = request.POST['from']
         b = request.POST['to']
-        print(a,b)
-        print("jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj")
         ans = a.split('-')
         bns = b.split('-')
         aa = datetime.date(int(ans[0]),int(ans[1]),int(ans[2]))
         bb = datetime.date(int(bns[0]),int(bns[1]),int(bns[2]))
         query_results = []
         last = []
-        # t = ['x']
         t = ['x', '00:00', '00:05', '00:10', '00:15', '00:20', '00:25', '00:30', '00:35', '00:40', '00:45', '00:50', '00:55', '01:00', '01:05', '01:10', '01:15', '01:20', '01:25', '01:30', '01:35', '01:40', '01:45', '01:50', '01:55', '02:00', '02:05', '02:10', '02:15', '02:20', '02:25', '02:30', '02:35', '02:40', '02:45', '02:50', '02:55', '03:00', '03:05', '03:10', '03:15', '03:20', '03:25', '03:30', '03:35', '03:40', '03:45', '03:50', '03:55', '04:00', '04:05', '04:10', '04:15', '04:20', '04:25', '04:30', '04:35', '04:40', '04:45', '04:50', '04:55', '05:00', '05:05', '05:10', '05:15', '05:20', '05:25', '05:30', '05:35', '05:40', '05:45', '05:50', '05:55', '06:00', '06:05', '06:10', '06:15', '06:20', '06:25', '06:30', '06:35', '06:40', '06:45', '06:50', '06:55', '07:00', '07:05', '07:10', '07:15', '07:20', '07:25', '07:30', '07:35', '07:40', '07:45', '07:50', '07:55', '08:00', '08:05', '08:10', '08:15', '08:20', '08:25', '08:30', '08:35', '08:40', '08:45', '08:50', '08:55', '09:00', '09:05', '09:10', '09:15', '09:20', '09:25', '09:30', '09:35', '09:40', '09:45', '09:50', '09:55', '10:00', '10:05', '10:10', '10:15', '10:20', '10:25', '10:30', '10:35', '10:40', '10:45', '10:50', '10:55', '11:00', '11:05', '11:10', '11:15', '11:20', '11:25', '11:30', '11:35', '11:40', '11:45', '11:50', '11:55', '12:00', '12:05', '12:10', '12:15', '12:20', '12:25', '12:30', '12:35', '12:40', '12:45', '12:50', '12:55', '13:00', '13:05', '13:10', '13:15', '13:20', '13:25', '13:30', '13:35', '13:40', '13:45', '13:50', '13:55', '14:00', '14:05', '14:10', '14:15', '14:20', '14:25', '14:30', '14:35', '14:40', '14:45', '14:50', '14:55', '15:00', '15:05', '15:10', '15:15', '15:20', '15:25', '15:30', '15:35', '15:40', '15:45', '15:50', '15:55', '16:00', '16:05', '16:10', '16:15', '16:20', '16:25', '16:30', '16:35', '16:40', '16:45', '16:50', '16:55', '17:00', '17:05', '17:10', '17:15', '17:20', '17:25', '17:30', '17:35', '17:40', '17:45', '17:50', '17:55', '18:00', '18:05', '18:10', '18:15', '18:20', '18:25', '18:30', '18:35', '18:40', '18:45', '18:50', '18:55', '19:00', '19:05', '19:10', '19:15', '19:20', '19:25', '19:30', '19:35', '19:40', '19:45', '19:50', '19:55', '20:00', '20:05', '20:10', '20:15', '20:20', '20:25', '20:30', '20:35', '20:40', '20:45', '20:50', '20:55', '21:00', '21:05', '21:10', '21:15', '21:20', '21:25', '21:30', '21:35', '21:40', '21:45', '21:50', '21:55', '22:00', '22:05', '22:10', '22:15', '22:20', '22:25', '22:30', '22:35', '22:40', '22:45', '22:50', '22:55', '23:00', '23:05', '23:10', '23:15', '23:20', '23:25', '23:30', '23:35', '23:40', '23:45', '23:50', '23:55']
         for i in range((bb-aa).days+1):
-            print(i ,"ayushJXHDKJHFKJDBH")
             query_results.append(CSV.objects.filter(date = aa+timedelta(days=i)).order_by('timestamp'))
             q = [str(aa+timedelta(days=i))]
             for x in query_results[i]:
-                # print(x.load_value, x.timestamp, x.date)
                 q.append(x.load_value)
             last.append(q)
-            print(len(q))
 
         last.insert(0,t)
 
@@ -69,29 +59,21 @@ def graph_plot(request):
 
 
 def forecasted_plot(request):
-    print("insde forecasted_plot");
     if request.method == 'POST':
-        print(request.POST)
         qq = request.POST['fc']
-        # p=5
-        # print(qq)
-        # print("qqqqq")
         ans = qq.split('-')
         aa = datetime.date(int(ans[0]),int(ans[1]),int(ans[2]))
         day = aa.day
         month = aa.month
         year = aa.year
         query_results = []
-        # last = []
         l = []
-        # for i in range(p+1):
         query_results.append((CSV.objects.filter(date = aa).order_by('timestamp')))
         q = [str(aa)]
         for x in query_results[0]:
             q.append(x.load_value)
-            # last.append(q)
+
         # weights = [0.8019, 0.0426, 0.0226, -0.0169, 0.1497]
-        # print(q)
         ARIMA_load = ['Forecasted with ARIMA']
         WMA_load = ['Forecasted with WMA']
         SMA_load = ['Forecasted with SMA']
@@ -101,10 +83,10 @@ def forecasted_plot(request):
         RNN_load = ['Forecasted with RNN']
 
         csv_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        print(str(day).zfill(2)+"-"+str(month).zfill(2)+"-"+str(year)+'.csv')
 
         arima_csv = pd.read_csv(os.path.join(csv_path, 'predictions/ARIMA/'+str(day).zfill(2)+"-"+str(month).zfill(2)+"-"+str(year)+'.csv'))['load'].values
         ARIMA_load.extend(list(arima_csv))
-        print(arima_csv)
         wma_csv = pd.read_csv(os.path.join(csv_path, 'predictions/WMA/'+str(day).zfill(2)+"-"+str(month).zfill(2)+"-"+str(year)+'.csv'))['load'].values
         WMA_load.extend(list(wma_csv))
 
@@ -134,41 +116,53 @@ def forecasted_plot(request):
         redu = q.count(None)
         length = len(q)-redu
         t = ['x', '00:00', '00:05', '00:10', '00:15', '00:20', '00:25', '00:30', '00:35', '00:40', '00:45', '00:50', '00:55', '01:00', '01:05', '01:10', '01:15', '01:20', '01:25', '01:30', '01:35', '01:40', '01:45', '01:50', '01:55', '02:00', '02:05', '02:10', '02:15', '02:20', '02:25', '02:30', '02:35', '02:40', '02:45', '02:50', '02:55', '03:00', '03:05', '03:10', '03:15', '03:20', '03:25', '03:30', '03:35', '03:40', '03:45', '03:50', '03:55', '04:00', '04:05', '04:10', '04:15', '04:20', '04:25', '04:30', '04:35', '04:40', '04:45', '04:50', '04:55', '05:00', '05:05', '05:10', '05:15', '05:20', '05:25', '05:30', '05:35', '05:40', '05:45', '05:50', '05:55', '06:00', '06:05', '06:10', '06:15', '06:20', '06:25', '06:30', '06:35', '06:40', '06:45', '06:50', '06:55', '07:00', '07:05', '07:10', '07:15', '07:20', '07:25', '07:30', '07:35', '07:40', '07:45', '07:50', '07:55', '08:00', '08:05', '08:10', '08:15', '08:20', '08:25', '08:30', '08:35', '08:40', '08:45', '08:50', '08:55', '09:00', '09:05', '09:10', '09:15', '09:20', '09:25', '09:30', '09:35', '09:40', '09:45', '09:50', '09:55', '10:00', '10:05', '10:10', '10:15', '10:20', '10:25', '10:30', '10:35', '10:40', '10:45', '10:50', '10:55', '11:00', '11:05', '11:10', '11:15', '11:20', '11:25', '11:30', '11:35', '11:40', '11:45', '11:50', '11:55', '12:00', '12:05', '12:10', '12:15', '12:20', '12:25', '12:30', '12:35', '12:40', '12:45', '12:50', '12:55', '13:00', '13:05', '13:10', '13:15', '13:20', '13:25', '13:30', '13:35', '13:40', '13:45', '13:50', '13:55', '14:00', '14:05', '14:10', '14:15', '14:20', '14:25', '14:30', '14:35', '14:40', '14:45', '14:50', '14:55', '15:00', '15:05', '15:10', '15:15', '15:20', '15:25', '15:30', '15:35', '15:40', '15:45', '15:50', '15:55', '16:00', '16:05', '16:10', '16:15', '16:20', '16:25', '16:30', '16:35', '16:40', '16:45', '16:50', '16:55', '17:00', '17:05', '17:10', '17:15', '17:20', '17:25', '17:30', '17:35', '17:40', '17:45', '17:50', '17:55', '18:00', '18:05', '18:10', '18:15', '18:20', '18:25', '18:30', '18:35', '18:40', '18:45', '18:50', '18:55', '19:00', '19:05', '19:10', '19:15', '19:20', '19:25', '19:30', '19:35', '19:40', '19:45', '19:50', '19:55', '20:00', '20:05', '20:10', '20:15', '20:20', '20:25', '20:30', '20:35', '20:40', '20:45', '20:50', '20:55', '21:00', '21:05', '21:10', '21:15', '21:20', '21:25', '21:30', '21:35', '21:40', '21:45', '21:50', '21:55', '22:00', '22:05', '22:10', '22:15', '22:20', '22:25', '22:30', '22:35', '22:40', '22:45', '22:50', '22:55', '23:00', '23:05', '23:10', '23:15', '23:20', '23:25', '23:30', '23:35', '23:40', '23:45', '23:50', '23:55']
-        l.insert(0,t)
+        l.insert(0, t)
     else:
         l = None
-
     print(l)
 
     def mean_absolute_percentage_error(y_pred, y_true):
-        y_true, y_pred = np.array(y_true), np.array(y_pred)
-        return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+        print('mape:', y_pred, y_true)
+        try:
+            y_true, y_pred = np.array(y_true), np.array(y_pred)
+            mape = np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+        except Exception as e:
+            mape = e
+        return mape
 
+    def root_mean_square_error(y_pred, y_true):
+        print('rmse:', y_pred, y_true)
+        try:
+            y_true, y_pred = np.array(y_true), np.array(y_pred)
+            rmse = np.sqrt((y_pred - y_true)**2).mean()
+        except Exception as e:
+            rmse = e
+        return rmse
 
 
     cont = {
             # "query_results":query_results,
-            'forecasted_Load':l,
-            'rmseSMA'  :round(np.sqrt(((np.array(l[1][1:length]) - np.array(l[8][1:length]))**2).mean()),2),
-            'rmseWMA'  :round(np.sqrt(((np.array(l[2][1:length]) - np.array(l[8][1:length]))**2).mean()),2),
-            'rmseSES'  :round(np.sqrt(((np.array(l[3][1:length]) - np.array(l[8][1:length]))**2).mean()),2),
-            'rmseARIMA':round(np.sqrt(((np.array(l[4][1:length]) - np.array(l[8][1:length]))**2).mean()),2),
-            'rmseLSTM' :round(np.sqrt(((np.array(l[5][1:length]) - np.array(l[8][1:length]))**2).mean()),2),
-            'rmseGRU'  :round(np.sqrt(((np.array(l[6][1:length]) - np.array(l[8][1:length]))**2).mean()),2),
-            'rmseRNN'  :round(np.sqrt(((np.array(l[7][1:length]) - np.array(l[8][1:length]))**2).mean()),2),
+            'forecasted_Load': l,
+            'rmseSMA': round(root_mean_square_error(l[1][1:length], l[8][1:length]), 2),
+            'rmseWMA': round(root_mean_square_error(l[2][1:length], l[8][1:length]), 2),
+            'rmseSES': round(root_mean_square_error(l[3][1:length], l[8][1:length]), 2),
+            'rmseARIMA': round(root_mean_square_error(l[4][1:length], l[8][1:length]), 2),
+            'rmseLSTM': round(root_mean_square_error(l[5][1:length], l[8][1:length]), 2),
+            'rmseGRU': round(root_mean_square_error(l[6][1:length], l[8][1:length]), 2),
+            'rmseRNN': round(root_mean_square_error(l[7][1:length], l[8][1:length]), 2),
 
-            'mapeSMA'  :round(mean_absolute_percentage_error(l[1][1:length], l[8][1:length]),2),
-            'mapeWMA'  :round(mean_absolute_percentage_error(l[2][1:length], l[8][1:length]),2),
-            'mapeSES'  :round(mean_absolute_percentage_error(l[3][1:length], l[8][1:length]),2),
-            'mapeARIMA':round(mean_absolute_percentage_error(l[4][1:length], l[8][1:length]),2),
-            'mapeLSTM' :round(mean_absolute_percentage_error(l[5][1:length], l[8][1:length]),2),
-            'mapeGRU'  :round(mean_absolute_percentage_error(l[6][1:length], l[8][1:length]),2),
-            'mapeRNN'  :round(mean_absolute_percentage_error(l[7][1:length], l[8][1:length]),2),
+            'mapeSMA': round(mean_absolute_percentage_error(l[1][1:length], l[8][1:length]),2),
+            'mapeWMA': round(mean_absolute_percentage_error(l[2][1:length], l[8][1:length]),2),
+            'mapeSES': round(mean_absolute_percentage_error(l[3][1:length], l[8][1:length]),2),
+            'mapeARIMA': round(mean_absolute_percentage_error(l[4][1:length], l[8][1:length]),2),
+            'mapeLSTM': round(mean_absolute_percentage_error(l[5][1:length], l[8][1:length]),2),
+            'mapeGRU': round(mean_absolute_percentage_error(l[6][1:length], l[8][1:length]),2),
+            'mapeRNN': round(mean_absolute_percentage_error(l[7][1:length], l[8][1:length]),2),
 
             # 'Tarikh':a,
             # 'T':mark_safe(t),
             # 'Load': [x.load_value for x in query_results],
             # 'Time': [x.timestamp for x in query_results],
         }
-
+    # print(cont)
     return HttpResponse(json.dumps(cont),content_type='application/json')
